9446a4d78e16d3be544c56871ff4a3fafc3ffa22
umarluqman <derezzed.78@gmail.com>
2021-10-03 07:55:37 +0800

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './style.css';
 import mecha from './mecha.json';
+import mechaDigitalEyes from './mecha.json';
 
 export default function App() {
-  const rareOne = mecha.results.filter(({ attributes }) => {
+  const inMagicEden = mecha.results.filter(({ attributes }) => {
     return attributes
       .map(({ value }) => {
         return value;
@@ -11,7 +12,17 @@ export default function App() {
       .includes('Wing_1_CARBON');
   });
 
-  console.log({ rareOne });
+  const inDigitalEyes = mechaDigitalEyes.offers.filter(
+    ({ metadata: { attributes } }) => {
+      return attributes
+        .map(({ value }) => {
+          return value;
+        })
+        .includes('Wing_1_CARBON');
+    }
+  );
+
+  console.log({ rareOne, inDigitalEyes });
 
   return (
     <div>
